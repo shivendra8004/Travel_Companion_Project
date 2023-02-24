2622a96b5a635787b13a3b5d58876036e9af5c12
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-24 19:52:15 +0530

    Imported all components in app.js and used material ui to style our code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-function App() {
+import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+    </>
   );
-}
+};
 export default App;
