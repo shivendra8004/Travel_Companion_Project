8da405a24a28da4d07ba190dbb8294b138ab07fd
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-24 19:15:16 +0530

    Removed unwanted code from app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Hello World</h1>
     </div>
   );
 }
-
 export default App;
