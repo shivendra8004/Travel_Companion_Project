6d0713a5faf4e638d2fd0805c2fcdd9f56a52afe
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-25 00:10:17 +0530

    successfully writen jsx code for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Autocomplete } from "@react-google-maps/api";
+// import { Autocomplete } from "@react-google-maps/api";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import useStyles from "./h_styles";
@@ -15,14 +15,14 @@ const Header = () => {
           <Typography varient="h6" className={classes.title}>
             Explore New Places
           </Typography>
-          <Autocomplete>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
             </div>
-          </Autocomplete>
+            <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+          </div>
+          {/* </Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar>
