aaea6504fd42564727e54ab20039de66d159e38f
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-24 19:56:29 +0530

    Added grid and map and styled them using material ui

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import { CssBaseline, Grid } from "@material-ui/core";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
-import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+// import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 const App = () => {
   return (
     <>
       <CssBaseline />
       <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Map />
+        </Grid>
+      </Grid>
     </>
   );
 };
