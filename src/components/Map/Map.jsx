353a1d41f6faa6275c6c426ffd79daa26f1ad436
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-25 20:05:46 +0530

    Added api to map but still not working

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
-// import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-// import { LocationOnOutlinedIcon } from "@material-ui/icons/LocationOnOutlined";
-// import Rating from "@material-ui/lab";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { LocationOnOutlinedIcon } from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
 import useStyles from "./m_styles";
 const Map = () => {
   const classes = useStyles();
-  // const isMobile = useMediaQuery("(min-width:600px)");
+  const isMobile = useMediaQuery("(min-width:600px)");
   const coordinates = { lat: 0, lng: 0 };
   return (
     <div className={classes.mapContainer}>
@@ -16,9 +16,9 @@ const Map = () => {
         center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        options={""}
-        onChange={""}
-        onChildClick={""}
+        // options={""}
+        // onChange={""}
+        // onChildClick={""}
       ></GoogleMapReact>
     </div>
   );
