c754be6c54b093a159b969fd0d54f99b19af9935
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-26 01:50:22 +0530

    Api started working

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
-import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-import { LocationOnOutlinedIcon } from "@material-ui/icons/LocationOnOutlined";
-import Rating from "@material-ui/lab";
+// import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+// import { LocationOnOutlinedIcon } from "@material-ui/icons/LocationOnOutlined";
+// import Rating from "@material-ui/lab";
 import useStyles from "./m_styles";
 const Map = () => {
   const classes = useStyles();
-  const isMobile = useMediaQuery("(min-width:600px)");
-  const coordinates = { lat: 0, lng: 0 };
+  // const isMobile = useMediaQuery("(min-width:600px)");
+  const coordinates = { lat: 24.4348, lng: 77.1609 };
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
+        bootstrapURLKeys={{ key: "AIzaSyCt-0-oRejXV9axIJkmB_LKfbp2dNkCCzM" }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        // options={""}
-        // onChange={""}
-        // onChildClick={""}
+        options={""}
+        onChange={""}
+        onChildClick={""}
       ></GoogleMapReact>
     </div>
   );
