98b3e7e48944d97124a97e85b4f0f954ff85ab5a
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-25 18:59:25 +0530

    Imported all basic components and styles in map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-
+import googleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOutlinedIcon from "@material-ui/icons/LocationOutlined";
+import Rating from "@material-ui/lab";
+import useStyles from "./m_styles";
 const Map = () => {
   return (
     <div>
