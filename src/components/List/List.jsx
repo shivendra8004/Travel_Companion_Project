5deb30fd2a97f555a69b87c1ed5410a5cc3c21a1
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-03 23:47:09 +0530

    Coding List

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-import { CircularProgress, Grid, Typography, Inputlabel, MenuItem, FormControl, Select } from "@material-ui/core";
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from "@material-ui/core";
 import useStyles from "./l_styles";
 const List = () => {
   const classes = useStyles();
 
-  return <div></div>;
+  return (
+    <div className={classes.container}>
+      <Typography varient="h4">Restaurants, Hotels & Attractions around you</Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attaractions">Attaractions</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
 export default List;
