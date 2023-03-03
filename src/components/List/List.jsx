40d3b8c259542d899e5969e04a4b4404bd877071
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-03 23:36:59 +0530

    Imported all required things in list.jsx file

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-
+import { CircularProgress, Grid, Typography, Inputlabel, MenuItem, FormControl, Select } from "@material-ui/core";
+import useStyles from "./l_styles";
 const List = () => {
-  return (
-    <div>
-      <h1>List</h1>
-    </div>
-  );
+  const classes = useStyles();
+
+  return <div></div>;
 };
 export default List;
