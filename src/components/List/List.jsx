2050f473f696877b808976cf758ca1146d563bc8
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-03 23:51:33 +0530

    Setting default value and onChange in list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from "@material-ui/core";
 import useStyles from "./l_styles";
 const List = () => {
   const classes = useStyles();
+  const [type, setType] = useState("attaractions");
 
   return (
     <div className={classes.container}>
       <Typography varient="h4">Restaurants, Hotels & Attractions around you</Typography>
       <FormControl className={classes.formControl}>
         <InputLabel>Type</InputLabel>
-        <Select>
+        <Select
+          value={type}
+          onChange={(e) => {
+            setType(e.target.value);
+          }}
+        >
           <MenuItem value="restaurants">Restaurants</MenuItem>
           <MenuItem value="hotels">Hotels</MenuItem>
-          <MenuItem value="attaractions">Attaractions</MenuItem>
+          <MenuItem value="attaractions">Attractions</MenuItem>
         </Select>
       </FormControl>
     </div>
