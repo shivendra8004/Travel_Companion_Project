07b353dfb47541edce97eeb7d48ae614b1d37c73
SHIVENDRA8004 <jat13632@gmail.com>
2023-05-01 13:41:27 +0530

    Google maps error fixed: Removed React Fragment from index.js

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,20 +7,20 @@ import useStyles from "./m_styles";
 const Map = () => {
   const classes = useStyles();
   // const isMobile = useMediaQuery("(min-width:600px)");
-  const coordinates = { lat: 24.4348, lng: 77.1609 };
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
-    },
-    zoom: 11,
-  };
+  // const coordinates = { lat: 24.4348, lng: 77.1609 };
+  // const defaultProps = {
+  //   center: {
+  //     lat: 10.99835602,
+  //     lng: 77.01502627,
+  //   },
+  //   zoom: 11,
+  // };
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyCt-0-oRejXV9axIJkmB_LKfbp2dNkCCzM" }}
-        defaultCenter={coordinates}
-        // defaultZoom={defaultProps.zoom}
+        defaultCenter={{ lat: 24.4348, lng: 77.1609 }}
+        defaultZoom={11}
       ></GoogleMapReact>
     </div>
   );
