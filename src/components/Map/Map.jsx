abb5c32e95e167eb17ba571032693122ff69e912
SHIVENDRA8004 <jat13632@gmail.com>
2023-05-01 12:18:35 +0530

    Developing Re start

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
-import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-import { LocationOnOutlinedIcon } from "@material-ui/icons/LocationOnOutlined";
-import Rating from "@material-ui/lab";
+// import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+// import { LocationOnOutlinedIcon } from "@material-ui/icons/LocationOnOutlined";
+// import Rating from "@material-ui/lab";
 import useStyles from "./m_styles";
 const Map = () => {
   const classes = useStyles();
-  const isMobile = useMediaQuery("(min-width:600px)");
+  // const isMobile = useMediaQuery("(min-width:600px)");
   const coordinates = { lat: 24.4348, lng: 77.1609 };
   const defaultProps = {
     center: {
