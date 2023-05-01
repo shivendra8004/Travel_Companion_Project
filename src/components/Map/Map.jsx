0562564457532216c87cf9dd3d176c4906fe36e4
SHIVENDRA8004 <jat13632@gmail.com>
2023-05-01 14:01:24 +0530

    Set environmental variables

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
-// import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-// import { LocationOnOutlinedIcon } from "@material-ui/icons/LocationOnOutlined";
-// import Rating from "@material-ui/lab";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { LocationOnOutlinedIcon } from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
 import useStyles from "./m_styles";
 const Map = () => {
   const classes = useStyles();
-  // const isMobile = useMediaQuery("(min-width:600px)");
-  // const coordinates = { lat: 24.4348, lng: 77.1609 };
-  // const defaultProps = {
-  //   center: {
-  //     lat: 10.99835602,
-  //     lng: 77.01502627,
-  //   },
-  //   zoom: 11,
-  // };
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const coordinates = { lat: 24.4348, lng: 77.1609 };
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyCt-0-oRejXV9axIJkmB_LKfbp2dNkCCzM" }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
         defaultCenter={{ lat: 24.4348, lng: 77.1609 }}
-        defaultZoom={11}
+        defaultZoom={defaultProps.zoom}
       ></GoogleMapReact>
     </div>
   );
