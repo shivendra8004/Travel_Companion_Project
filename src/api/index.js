d1cdda1eb533411ed636f440a822d8e80dd1d8db
SHIVENDRA8004 <jat13632@gmail.com>
2023-05-02 10:02:47 +0530

    Seted env variables and removed the code that is unused for now

--- src/api/index.js
+++ src/api/index.js
@@ -1,34 +1,18 @@
 import axios from "axios";
-
+const URL = "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
 // Copied Code From Travel Advisor Rapid API Page
 const options = {
-  method: "GET",
-  url: "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary",
   params: {
     bl_latitude: "11.847676",
     tr_latitude: "12.838442",
     bl_longitude: "109.095887",
     tr_longitude: "109.149359",
-    restaurant_tagcategory_standalone: "10591",
-    restaurant_tagcategory: "10591",
-    limit: "30",
-    currency: "USD",
-    open_now: "false",
-    lunit: "km",
-    lang: "en_US",
   },
   headers: {
-    "X-RapidAPI-Key": "6f6bd3f42cmsh8e4658f89a4755cp150426jsn80fceb2cfaf3",
+    "X-RapidAPI-Key": process.env.TRAVEL_ADVISOR_API_KEY,
     "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
   },
 };
-
-try {
-  const response = await axios.request(options);
-  console.log(response.data);
-} catch (error) {
-  console.error(error);
-}
 
 const getPlacesDetails = async () => {
   try {
