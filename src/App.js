2e26f870f5cc8321738d81e1420fb96715ea7a18
SHIVENDRA8004 <jat13632@gmail.com>
2023-05-02 10:36:13 +0530

    Updated all code and created states for places,coordinates and bounds

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// All Import Statements
 import React, { useEffect, useState } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 import Header from "./components/Header/Header";
@@ -5,12 +6,21 @@ import List from "./components/List/List";
 import Map from "./components/Map/Map";
 import { getPlacesDetails } from "./api";
 import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+
+// All Functions
 const App = () => {
+  // All useStates and constants
   const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState(null);
+
+  // This state is to update the boundaries of map when we drag using mouse in map
+  const [bounds, setBounds] = useState({});
+
+  // All useEffect functions
   useEffect(() => {
-    getPlacesDetails().then((places) => {
-      console.log(places);
-      setPlaces(places);
+    getPlacesDetails().then((data) => {
+      console.log(data);
+      setPlaces(data);
     });
   }, []);
   return (
@@ -19,7 +29,7 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map />
@@ -28,4 +38,6 @@ const App = () => {
     </>
   );
 };
+
+// Exporting functions
 export default App;
