5a68447a8c9ae991ba59286be1a9c8d14fe7f386
SHIVENDRA8004 <jat13632@gmail.com>
2023-05-04 19:47:35 +0530

    Now creating a good card for displaying details of restaurants

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from "@material-ui/core";
+import { LocationOn, Phone } from "@material-ui/icons";
+import { Rating } from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const PlaceDetails = ({ place }) => {
-  const name = place.name;
-  return (
-    <div>
-      <h1>{name}</h1>
-    </div>
-  );
+    const name = place.name;
+    return (
+        <div>
+            <h1>{name}</h1>
+        </div>
+    );
 };
 export default PlaceDetails;
