c68c46280262741a03279ec6c9c7032a5204b987
SHIVENDRA8004 <jat13632@gmail.com>
2023-05-06 01:59:53 +0530

    Passed places data to map component and Locating all those places on map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,32 +1,46 @@
 // All import statements will be added here
 import React from "react";
 import GoogleMapReact from "google-map-react";
-// import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-// import { LocationOnOutlinedIcon } from "@material-ui/icons/LocationOnOutlined";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 // import Rating from "@material-ui/lab";
 import useStyles from "./styles";
 
 // All Functions will be declared here
-const Map = ({ setCoordinates, setBounds, coordinates }) => {
-  const classes = useStyles();
-  // const isMobile = useMediaQuery("(min-width:600px)");
+const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery("(min-width:600px)");
 
-  return (
-    <div className={classes.mapContainer}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
-        defaultCenter={coordinates}
-        defaultZoom={14}
-        margin={[50, 50, 50, 50]}
-        options={""}
-        onChange={(e) => {
-          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
-          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
-          console.log(e.marginBounds.sw, e.marginBounds.ne, "Bounds");
-        }}
-        onChildClick={""}
-      ></GoogleMapReact>
-    </div>
-  );
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+                defaultCenter={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={""}
+                onChange={(e) => {
+                    setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+                    console.log(e.marginBounds.sw, e.marginBounds.ne, "Bounds");
+                }}
+                onChildClick={""}
+            >
+                {places?.map((place, i) => (
+                    <div className={classes.markerContainer} lat={Number(place.latitude)} lng={Number(place.longitude)} key={i}>
+                        {isMobile ? (
+                            <LocationOnOutlinedIcon color="primary" fontSize="large" />
+                        ) : (
+                            <Paper elevation={3} className={classes.paper}>
+                                <Typography className={classes.typography} variant="subtitle2 " gutterBottom>
+                                    {place.name}
+                                </Typography>
+                            </Paper>
+                        )}
+                    </div>
+                ))}
+            </GoogleMapReact>
+        </div>
+    );
 };
 export default Map;
