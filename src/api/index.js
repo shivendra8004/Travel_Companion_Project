dcadcfb16759ecef78c63e3862f0c16f42c4d079
SHIVENDRA8004 <jat13632@gmail.com>
2023-05-06 00:35:37 +0530

    API request limit reached: Updated new key

--- src/api/index.js
+++ src/api/index.js
@@ -2,23 +2,23 @@ import axios from "axios";
 const URL = "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
 
 export const getPlacesDetails = async (sw, ne) => {
-  try {
-    const {
-      data: { data },
-    } = await axios.get(URL, {
-      params: {
-        bl_latitude: sw.lat,
-        tr_latitude: ne.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-      },
-      headers: {
-        "X-RapidAPI-Key": "6f6bd3f42cmsh8e4658f89a4755cp150426jsn80fceb2cfaf3",
-        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
-      },
-    });
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const {
+            data: { data },
+        } = await axios.get(URL, {
+            params: {
+                bl_latitude: sw.lat,
+                tr_latitude: ne.lat,
+                bl_longitude: sw.lng,
+                tr_longitude: ne.lng,
+            },
+            headers: {
+                "X-RapidAPI-Key": "97deaa588fmsh3902de034442ba0p120a22jsn63d56ab47423",
+                "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+            },
+        });
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
 };
