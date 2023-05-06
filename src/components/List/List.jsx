a90b97067cc95ce54d5240067ab442605e6d8aa8
SHIVENDRA8004 <jat13632@gmail.com>
2023-05-07 01:29:44 +0530

    Added rest all code in else statement in list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -23,44 +23,45 @@ const List = ({ places, childClick, loading }) => {
                     <CircularProgress size="5rem" />
                 </div>
             ) : (
-                ""
-            )}
-            {/* Form Control for select places */}
-            <FormControl className={classes.formControl}>
-                <InputLabel>Type</InputLabel>
-                <Select
-                    value={type}
-                    onChange={(e) => {
-                        setType(e.target.value);
-                    }}
-                >
-                    <MenuItem value="restaurants">Restaurants</MenuItem>
-                    <MenuItem value="hotels">Hotels</MenuItem>
-                    <MenuItem value="attractions">Attractions</MenuItem>
-                </Select>
-            </FormControl>
-            {/* Form control for Ratings */}
-            <FormControl className={classes.formControl}>
-                <InputLabel>Rating</InputLabel>
-                <Select
-                    value={rating}
-                    onChange={(e) => {
-                        setRating(e.target.value);
-                    }}
-                >
-                    <MenuItem value="0">All</MenuItem>
-                    <MenuItem value="3">Above 3.0</MenuItem>
-                    <MenuItem value="4">Above 4.0</MenuItem>
-                    <MenuItem value="4.5">Above 4.5</MenuItem>
-                </Select>
-            </FormControl>
-            <Grid container spacing={3} className={classes.list}>
-                {places?.map((place, i) => (
-                    <Grid item key={i} xs={12} md={12}>
-                        {place.name ? <PlaceDetails selected={Number(childClick === i)} refProp={elementRefs[i]} place={place} /> : ""}
+                <>
+                    {/* Form Control for select places */}
+                    <FormControl className={classes.formControl}>
+                        <InputLabel>Type</InputLabel>
+                        <Select
+                            value={type}
+                            onChange={(e) => {
+                                setType(e.target.value);
+                            }}
+                        >
+                            <MenuItem value="restaurants">Restaurants</MenuItem>
+                            <MenuItem value="hotels">Hotels</MenuItem>
+                            <MenuItem value="attractions">Attractions</MenuItem>
+                        </Select>
+                    </FormControl>
+                    {/* Form control for Ratings */}
+                    <FormControl className={classes.formControl}>
+                        <InputLabel>Rating</InputLabel>
+                        <Select
+                            value={rating}
+                            onChange={(e) => {
+                                setRating(e.target.value);
+                            }}
+                        >
+                            <MenuItem value="0">All</MenuItem>
+                            <MenuItem value="3">Above 3.0</MenuItem>
+                            <MenuItem value="4">Above 4.0</MenuItem>
+                            <MenuItem value="4.5">Above 4.5</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <Grid container spacing={3} className={classes.list}>
+                        {places?.map((place, i) => (
+                            <Grid item key={i} xs={12} md={12}>
+                                {place.name ? <PlaceDetails selected={Number(childClick === i)} refProp={elementRefs[i]} place={place} /> : ""}
+                            </Grid>
+                        ))}
                     </Grid>
-                ))}
-            </Grid>
+                </>
+            )}
         </div>
     );
 };
